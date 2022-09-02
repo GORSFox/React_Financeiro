091b741f86471d64aac3eb8715e6ee1fc86e6d2e
Gors <gors1315@gmail.com>
2022-09-02 18:49:51 -0300

    first

---
+++ src/Components/Resume/styles.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const container = styled.div`
+max-width: 1120px;
+width: 98%;
+margin: 0 auto;
+display: flex;
+gap: 20px; /*espaços entre os cards */
+margin-top: -50px;
+justify-content: space-around;
+`;
+
+
+
